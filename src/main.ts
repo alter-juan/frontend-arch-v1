43fcb715d5afb90570efdd2f4efd118c642e941d
alter-juan <sebastian.peralta@alternova.com>
2024-04-03 14:49:34 -0500

    chore: Config route and remove registratio modules

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,5 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { router } from './routes'
-import { UserRouter } from './user/User.routes'
-
-UserRouter(router)
 
 createApp(App).use(router).mount('#app')
