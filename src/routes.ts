6983241e1ac6e9bf786451a2ad4ae2fb7913dcc8
alter-juan <sebastian.peralta@alternova.com>
2024-04-04 13:54:56 -0500

    feat: add auth routes to app

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { createWebHistory, createRouter, RouteRecordRaw } from "vue-router";
 import UserRoutes  from "./user/user.routes";
+import AuthRoutes  from "./auth/auth.routes";
 
 const routes : RouteRecordRaw[] = [
   {
@@ -11,6 +12,7 @@ const routes : RouteRecordRaw[] = [
     component: () => import("./core/views/NotFoundView.vue"),
   },
   UserRoutes, 
+  AuthRoutes
 ];
 
 export const router = createRouter({
