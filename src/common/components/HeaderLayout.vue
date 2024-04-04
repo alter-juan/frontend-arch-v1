7aa5f3dfa20adf439013013d1b5cfcd00c542c32
alter-juan <sebastian.peralta@alternova.com>
2024-04-04 13:48:28 -0500

    chore: add header component and add common errors

---
+++ src/common/components/HeaderLayout.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { AuthRoutes } from "../../auth";
+</script>
+<template>
+       <header>
+            <h1>Vue template</h1>
+            <router-link :to="{ name: AuthRoutes.LOGIN }">Sign In</router-link>
+        </header>
+</template>
+<style scoped>
+    header {
+        background-color: #242424;
+        padding: 1em 1.5em;
+        margin: 0 2rem;
+        position: sticky;
+        top: 0;
+        z-index: 2;
+        border: #82d39d;
+        border-bottom: solid #82d39d 2px;
+        text-align: center;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        h1 {
+            margin: 0;
+            color: #82d39d;
+            font-size: 1.5rem;
+        }
+
+        a {
+            color: #82d39d;
+            text-decoration: none;
+            margin-left: 1em;
+        }
+        
+        a:hover {
+            text-decoration: underline;
+        }
+    }
+</style>
