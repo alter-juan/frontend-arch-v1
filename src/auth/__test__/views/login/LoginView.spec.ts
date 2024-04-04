a682837d5689ca9206b54c7a6759db5aeec27a0a
alter-juan <sebastian.peralta@alternova.com>
2024-04-04 17:47:46 -0500

    🧪 test: Add LoginView unit tests and mock AuthRestCommand

---
+++ src/auth/__test__/views/login/LoginView.spec.ts
@@ -0,0 +1,75 @@
+import { render, screen } from "@testing-library/vue";
+import LoginView from "../../../views/login/LoginView.vue";
+import userEvent from '@testing-library/user-event';
+import { faker } from "@faker-js/faker";
+import { useRouter } from "vue-router";
+
+vi.mock('vue-router', () => ({
+    useRouter: vi.fn().mockReturnValue({
+        push: vi.fn(),
+    }),
+}));
+
+vi.mock('../../../services/authRest.command.ts');
+
+describe('Given a view to login', () =>{
+    beforeEach(() => {
+        vi.clearAllMocks();
+    });
+
+    describe('When user is on login page', () => {
+        it('Then user must see a title', () => {
+            // Arrange
+            render(LoginView);
+            
+            // Act
+            const title = screen.getByRole("heading", {
+                name: /Sign in/i,
+            });
+
+            // Assert
+            expect(title).toBeTruthy();
+        });
+
+        it('Then user must see a form with email, password and submit button', () => {
+            // Arrange
+            render(LoginView);
+            
+            // Act
+            const emailInput = screen.getByLabelText(/Email/i);
+            const passwordInput = screen.getByLabelText(/Password/i);
+            const usernameInput = screen.getByLabelText(/Username/i);
+            const submitButton = screen.getByRole("button", { name: /Sign in/i });
+
+            // Assert
+
+            expect(emailInput).toBeTruthy();
+            expect(passwordInput).toBeTruthy();
+            expect(usernameInput).toBeTruthy();
+            expect(submitButton).toBeTruthy();
+        });
+    });
+
+    describe('When user fills the form and submits', () => {
+        it('Then user must see a message that login was successful', async () => {
+            // Arrange
+            const user = userEvent.setup();
+            render(LoginView);
+
+            const emailInput = screen.getByLabelText(/Email/i);
+            const passwordInput = screen.getByLabelText(/Password/i);
+            const usernameInput = screen.getByLabelText(/Username/i);
+            const submitButton = screen.getByRole("button", { name: /Sign in/i });
+
+            // Act
+            await user.type(emailInput, faker.internet.email());
+            await user.type(usernameInput, faker.internet.userName());
+            await user.type(passwordInput, faker.internet.password());
+
+            await user.click(submitButton);
+
+            // Assert
+            expect(useRouter().push).toHaveBeenCalledWith("/");
+        });
+    });
+});
---
+++ src/auth/services/__mocks__/authRest.command.ts
@@ -0,0 +1,8 @@
+import { LoginPayload } from "../../models/entity";
+import { AuthCommand } from "../auth.repository";
+
+export class AuthRestCommand implements AuthCommand {
+    async login(payload: LoginPayload): Promise<void> {
+       console.info("Login request sent", payload);
+    }
+}
