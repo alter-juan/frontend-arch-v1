cae1fd94beb5dcce141f7096ea3eb26e05bef37f
alter-juan <sebastian.peralta@alternova.com>
2024-04-05 17:18:35 -0500

    chore: Add logic to refresh token

--- src/core/client/client.ts
+++ src/core/client/client.ts
@@ -6,15 +6,43 @@ const client = axios.create({
 
 client.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token");
+    const token = sessionStorage.getItem("token");
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
     }
     return config;
-  },
-  (error) => {
-    return Promise.reject(error);
   }
 );
+
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if(error.response.status === 401) {
+     /** 
+      * Refresh token logic
+      * 
+      * This is a simplified version of the refresh token logic using a dummy api,
+      *  in a real-world scenario you would need to handle the refresh token
+      *  logic in a more secure way and  avoid remove the token with the
+      *  location.reload() method.
+     */
+      sessionStorage.removeItem("token");
+      window.location.reload();
+
+      const { data, status } = await client.post("auth/refresh", {
+        expiresInMins: 5,
+      }); 
+      
+      if (status === 200) {
+        sessionStorage.setItem("token", data.token);
+        return client.request(error.config);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+)
 
 export default client;
